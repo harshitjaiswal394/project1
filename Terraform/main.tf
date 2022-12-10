9d6c0f0a88a052be7d0002ff93fd0643037c4f3a
harshitjaiswal394 <harshitjaiswal394@hotmail.com>
2022-12-10 20:57:48 +0530

    final commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "ubuntu-linux-2004" {
 }
 
 resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.ubuntu-linux-2004.id
+  ami = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name = "jenkins2"
 
