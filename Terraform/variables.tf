cb7d6807fa0ea0b397aef921dc61d114ebfd7a26
harshitjaiswal394 <harshitjaiswal394@hotmail.com>
2022-12-12 14:40:10 +0530

    final commit

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -4,5 +4,5 @@ variable "name" {
 variable "instance_keypair" {
       description = "Instance key-pair that need to be associated with EC2 Instance"
       type = string
-      default = "jenkins2"
+      default = "jenkin2"
     }
