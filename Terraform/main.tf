d8193cd091f46bc7311c6b0ee4ce18e91daa9e29
harshitjaiswal394 <harshitjaiswal394@hotmail.com>
2022-12-15 15:22:55 +0530

    final commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,37 @@
+provider "aws" {
+  region = var.aws_region
+}
+#Create security group with firewall rules
+resource "aws_security_group" "my_security_group" {
+  name        = var.security_group
+  description = "security group for Ec2 instance"
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -15,11 +49,19 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name = var.instance_keypairkey
-
+  instance_type = var.instance_type
+  key_name = var.key_name
+  security_groups= var.security_group
   tags = {
     Environment = "dev"
     Name = "${var.name}-server"
+  }
+}
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
   }
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,8 +1,25 @@
 variable "name" {
   default = ""
 }
-variable "instance_keypairkey" {
-      description = "Instance key-pair that need to be associated with EC2 Instance"
-      type = string
-      default = "jenkin2"
-    }
+
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "jenkin2"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "jenkins-security-group"
+}
+
+
